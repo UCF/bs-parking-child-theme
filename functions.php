7304c97e8207a983564b39ef89772e1a4aa0d7d5
Mike Setzer <michaelsetzerucf@gmail.com>
2023-10-05 14:34:00 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,12 +1,12 @@
 <?php
 //namespace parking\Theme;
 
-define( 'PARKING_THEME_DIR', trailingslashit( get_stylesheet_directory() ) );
+//define( 'PARKING_THEME_DIR', trailingslashit( get_stylesheet_directory() ) );
 
 
 // Theme foundation
-include_once PARKING_THEME_DIR . 'includes/config.php';
-include_once PARKING_THEME_DIR . 'includes/meta.php';
+include_once 'includes/config.php';
+include_once 'includes/meta.php';
 
 // Add other includes to this file as needed.
 
