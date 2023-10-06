412ff4831ada42382e4a3ed55dcd56d357ca9980
Mike Setzer <michaelsetzerucf@gmail.com>
2023-10-06 09:29:47 -0400

    Adjustments for jQuery

--- functions.php
+++ functions.php
@@ -9,8 +9,3 @@ include_once 'includes/config.php';
 include_once 'includes/meta.php';
 
 // Add other includes to this file as needed.
-
-function register_custom_scripts() {
-	wp_register_script( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js', array(), null, true );
-}
-add_action( 'wp_enqueue_scripts', 'register_custom_scripts' );
--- includes/meta.php
+++ includes/meta.php
@@ -13,8 +13,8 @@ function enqueue_frontend_assets() {
 	$theme = wp_get_theme();
 	$theme_version = $theme->get( 'Version' );
 
+	wp_enqueue_script('jquery');
 	wp_enqueue_style( 'style-child', PARKING_THEME_CSS_URL . '/style.min.css', array( 'style' ), $theme_version );
 	wp_enqueue_script( 'script-child', PARKING_THEME_JS_URL . '/script.min.js', array( 'jquery', 'script' ), $theme_version, true );
 }
-
 add_action( 'wp_enqueue_scripts', 'enqueue_frontend_assets', 11 );
